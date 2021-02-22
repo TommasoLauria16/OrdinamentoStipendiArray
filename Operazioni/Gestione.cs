1c5dec17d19b53532a34a693e1889e7164b1880b
tommaso lauria <tommaso.lauria9@gmail.com>
2021-02-22 10:37:10 +0100

    Add project files.

---
+++ Operazioni/Gestione.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operazioni
+{
+    public class Gestione
+    {
+        public static void Riempimento(double[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"inserisci l'elemento in posizione {i+1}");
+                array[i] = double.Parse(Console.ReadLine());
+
+            }
+        }
+        public static void BubbleSort1(double[] array)
+        {
+            int x, y;
+            double temp;
+            for (x = 0; x < array.Length - x; x++)
+            {
+                for (y = 0; y < array.Length - 1; y++)
+                {
+                    if (array[y] > array[y + 1])
+                    {
+                        temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void Stampa(double[] array)
+        {
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine($"{array[array.Length - 1]}.");
+        }
+
+    }
+}
---
+++ OrdinamentoStipendiMaxMin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Operazioni;
+
+namespace OrdinamentoStipendiMaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci la dimensione dell'array");
+            int k = int.Parse(Console.ReadLine());
+            double[] array = new double[k];
+            Gestione.Riempimento(array);
+            Gestione.BubbleSort1(array);
+            Console.WriteLine($"il min è {array[0]}.");
+            Console.WriteLine($"il max è {array[array.Length-1]}.");
+            Console.Write("l'array ordinato è: ");
+            Gestione.Stampa(array);
+
+
+        }
+    }
+}
